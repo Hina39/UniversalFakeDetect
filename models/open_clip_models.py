66e238fa1e04b1c2b583174a091b8116f9ea3b17
Hina39 <hiinyan3939@gmail.com>
2024-06-15 00:26:15 +0900

    :rose: add open clip model

---
+++ models/open_clip_models.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import open_clip
+
+
+class OpenCLIPModel(nn.Module):
+    def __init__(
+        self,
+        name,
+        num_classes=1,
+        model_name="ViT-B-16",
+        pretrained="laion400m_e31",
+        channels=512,
+    ):
+        super(OpenCLIPModel, self).__init__()
+        model, _, preprocess = open_clip.create_model_and_transforms(
+            model_name, pretrained=pretrained
+        )
+        self.model = model
+        self.fc = nn.Linear(channels, num_classes)
+
+    def forward(self, x, return_feature=False):
+        features = self.model.encode_image(x)
+        if return_feature:
+            return features
+        return self.fc(features)
